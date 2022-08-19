2883f5b54c93f99e5d8e8fb30c45c4b1d4c6472b
gatlace <zii.donato@outlook.com>
2022-08-19 14:51:32 -0400

    changed procfile

--- backend/runscript.py
+++ backend/runscript.py
@@ -3,6 +3,6 @@ import os
 
 import os
 
-os.system("pip install -r requirements.txt")
 os.system("./bin/activate")
+os.system("pip install -r requirements.txt")
 os.system("python manage.py runserver")
