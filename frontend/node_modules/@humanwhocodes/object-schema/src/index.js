0ee7945d6a820542a5826764f8eeeb885c06f45b
gatlace <zii.donato@outlook.com>
2022-09-01 15:44:04 -0400

    edited node_modules

--- frontend/node_modules/@humanwhocodes/object-schema/src/index.js
+++ frontend/node_modules/@humanwhocodes/object-schema/src/index.js
@@ -4,5 +4,4 @@
 
 exports.ObjectSchema = require("./object-schema").ObjectSchema;
 exports.MergeStrategy = require("./merge-strategy").MergeStrategy;
-exports.ValidationStrategy =
-  require("./validation-strategy").ValidationStrategy;
+exports.ValidationStrategy = require("./validation-strategy").ValidationStrategy;
--- frontend/node_modules/@humanwhocodes/object-schema/tests/merge-strategy.js
+++ frontend/node_modules/@humanwhocodes/object-schema/tests/merge-strategy.js
@@ -17,40 +17,50 @@ const { MergeStrategy } = require("../src/");
 //-----------------------------------------------------------------------------
 
 describe("MergeStrategy", () => {
-  describe("overwrite()", () => {
-    it("should overwrite the first value with the second when the second is defined", () => {
-      const result = MergeStrategy.overwrite(1, 2);
-      assert.strictEqual(result, 2);
+
+
+    describe("overwrite()", () => {
+
+        it("should overwrite the first value with the second when the second is defined", () => {
+            const result = MergeStrategy.overwrite(1, 2);
+            assert.strictEqual(result, 2);
+        });
+
+        it("should overwrite the first value with the second when the second is undefined", () => {
+            const result = MergeStrategy.overwrite(1, undefined);
+            assert.strictEqual(result, undefined);
+        });
+
     });
 
-    it("should overwrite the first value with the second when the second is undefined", () => {
-      const result = MergeStrategy.overwrite(1, undefined);
-      assert.strictEqual(result, undefined);
-    });
-  });
+    describe("replace()", () => {
 
-  describe("replace()", () => {
-    it("should overwrite the first value with the second when the second is defined", () => {
-      const result = MergeStrategy.replace(1, 2);
-      assert.strictEqual(result, 2);
+        it("should overwrite the first value with the second when the second is defined", () => {
+            const result = MergeStrategy.replace(1, 2);
+            assert.strictEqual(result, 2);
+        });
+
+        it("should return the first value when the second is undefined", () => {
+            const result = MergeStrategy.replace(1, undefined);
+            assert.strictEqual(result, 1);
+        });
+
     });
 
-    it("should return the first value when the second is undefined", () => {
-      const result = MergeStrategy.replace(1, undefined);
-      assert.strictEqual(result, 1);
-    });
-  });
+    describe("assign()", () => {
 
-  describe("assign()", () => {
-    it("should merge properties from two objects when called", () => {
-      const object1 = { foo: 1, bar: 3 };
-      const object2 = { foo: 2 };
+        it("should merge properties from two objects when called", () => {
 
-      const result = MergeStrategy.assign(object1, object2);
-      assert.deepStrictEqual(result, {
-        foo: 2,
-        bar: 3,
-      });
+            const object1 = { foo: 1, bar: 3 };
+            const object2 = { foo: 2 };            
+            
+            const result = MergeStrategy.assign(object1, object2);
+            assert.deepStrictEqual(result, {
+                foo: 2,
+                bar: 3
+            });
+        });
+
     });
-  });
+
 });
