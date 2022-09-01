ed1ec5379159783c453b6d15a73d5a3feea38d4d
gatlace <zii.donato@outlook.com>
2022-09-01 15:47:44 -0400

    removed node_modules

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3211054efde5ac6"; }
+/******/ 		__webpack_require__.h = function() { return "9252224e22fcd73a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
